c801cf3fc5ca30b2b7bff3f04799336f35fdd9c3
votruongthinh <143499278+votruongthinh@users.noreply.github.com>
2026-07-15 09:28:23 +0700

    kiểm tra và xuất kết quả bài 1 và bài 2

--- bai1.dart
+++ bai1.dart
@@ -1,3 +1,41 @@
 void main(){
-    
+    String hoTen = "Võ Trường Thịnh";
+    double soGioLam = 45;
+    double luongMoiGio = 300000;
+
+    //tính tổng lương 
+    double tongLuong = soGioLam * luongMoiGio;
+
+    //2.Phụ cấp
+    double phuCap = 0;
+
+    //2.1 kiểm tra nếu số giờ làm trên 40 giờ thì thưởng thêm 20%
+    if(soGioLam > 40){
+        phuCap = tongLuong * 0.2;
+    }
+
+    double luongTruocThue = tongLuong + phuCap;
+
+    //3.Thuế
+    double thue = 0;
+
+    if(luongTruocThue > 10000000){
+        thue = luongTruocThue * 0.1;
+
+    }else if(luongTruocThue >= 7000000){
+        thue = luongTruocThue * 0.05;
+    }else{
+        thue = 0;
+    }
+
+    //lương thực lãnh 
+    double luongThucLanh = luongTruocThue - thue;
+
+    print("======= Thông tin nhân viên =======");
+    print("họ và tên : $hoTen");
+    print("Tổng lương :$tongLuong");
+    print("Phụ cấp: $phuCap");
+    print("Luong trước thuế: $luongTruocThue");
+    print("Thuế:$thue");
+    print("Lương thực lãnh:$luongThucLanh");
 }
--- bai2.dart
+++ bai2.dart
@@ -1,3 +1,37 @@
 void main(){
-    
+    String tenSanPham = "Lap Top";
+    int soLuongMua = 2;
+    double donGia = 650000;
+
+    //1 thành tiền 
+    double thanhTien = soLuongMua * donGia;
+
+    //2 giảm giá
+    double giamGia = 0;
+    if(thanhTien >= 1000000){
+        giamGia = thanhTien * 0.1;
+    }else if(thanhTien >= 500000){
+        giamGia = thanhTien * 0.05;
+    }else{
+        giamGia = 0;
+    }
+
+    // tiền sau giảm giá
+    double tienSauGiam = thanhTien - giamGia;
+
+    //3.thuế 
+    double vat = tienSauGiam * 0.08;
+
+    //tổng thanh toán 
+    double tongThanhToan = tienSauGiam + vat;
+
+      // 4. In hóa đơn
+     print("===== HÓA ĐƠN =====");
+    print("Tên sản phẩm: $tenSanPham");
+     print("Số lượng: $soLuongMua");
+    print("Đơn giá: $donGia");
+     print("Thành tiền: $thanhTien");
+    print("Giảm giá: $giamGia");
+    print("VAT: $vat");
+    print("Tổng thanh toán: $tongThanhToan");
 }
