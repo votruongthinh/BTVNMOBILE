e2c446e2925c3700f90859ad8e55280375b7f532
votruongthinh <143499278+votruongthinh@users.noreply.github.com>
2026-07-30 10:26:08 +0700

    bai2 oop

--- bai2.dart
+++ bai2.dart
@@ -1,37 +1,35 @@
-void main(){
-    String tenSanPham = "Lap Top";
-    int soLuongMua = 2;
-    double donGia = 650000;
+void main() {
+  Product p = Product(name:"laptop",quality:10,price:10000);
+  p.printBill();
+}
 
-    //1 thành tiền 
-    double thanhTien = soLuongMua * donGia;
+class Product {
+  final String name;
+  final double quality;
+  final double price;
 
-    //2 giảm giá
-    double giamGia = 0;
-    if(thanhTien >= 1000000){
-        giamGia = thanhTien * 0.1;
-    }else if(thanhTien >= 500000){
-        giamGia = thanhTien * 0.05;
-    }else{
-        giamGia = 0;
+  Product({required this.name, required this.quality, required this.price});
+
+  double total(){
+    return quality * price;
+  }
+  double discount(){
+    if(total() >1000000){
+      return total() * 0.1;
+    }else if(total()>=500000){
+      return total() * 0.05;
     }
-
-    // tiền sau giảm giá
-    double tienSauGiam = thanhTien - giamGia;
-
-    //3.thuế 
-    double vat = tienSauGiam * 0.08;
-
-    //tổng thanh toán 
-    double tongThanhToan = tienSauGiam + vat;
-
-    // 4. In hóa đơn
-    print("===== HÓA ĐƠN =====");
-    print("Tên sản phẩm: $tenSanPham");
-    print("Số lượng: $soLuongMua");
-    print("Đơn giá: $donGia");
-    print("Thành tiền: $thanhTien");
-    print("Giảm giá: $giamGia");
-    print("VAT: $vat");
-    print("Tổng thanh toán: $tongThanhToan");
+    return 0;
+  }
+  double finalTotal(){
+    return total() -((total()- discount()) * 0.08);
+  }
+  void printBill(){
+    print("ten san pham:$name");
+    print("so luong san pham:$quality");
+    print("gia san pham:$price");
+    print("tong tien:${total()}");
+    print("giam gia:${discount()}");
+    print("Tong cuoi cung:${finalTotal()}");
+  }
 }
